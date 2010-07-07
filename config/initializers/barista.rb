400523c5e688b35713265c11ff167405a73b98af
Darcy Laycock <sutto@sutto.net>
2010-07-07 12:36:47 +0800

    add specific path

--- config/initializers/barista.rb
+++ config/initializers/barista.rb
@@ -2,3 +2,5 @@ Barista.configure do |c|
   c.no_wrap = true
   c.change_output_prefix! 'shuriken', 'vendor/shuriken'
 end
+# Grrr!
+Barista::Compiler.bin_path = "/usr/local/bin/coffee" if Rails.env.production?
